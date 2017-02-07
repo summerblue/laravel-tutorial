24b30aca5634f6a462629eb5f36633156e288f1f
summerblue <cj@estgroupe.com>
2017-02-07 21:33:53 +0800

    Improve user seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,12 +13,14 @@
 
 $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
     $date_time = $faker->date . ' ' . $faker->time;
+    static $password;
+
     return [
         'name' => $faker->name,
         'email' => $faker->safeEmail,
         'is_admin' => false,
         'activated' => true,
-        'password' => str_random(10),
+        'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
         'created_at' => $date_time,
         'updated_at' => $date_time,
